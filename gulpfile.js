f1377c669cfce0b85d3ead0d23848a8af173ef9a
Justin Willis <justinwillis@Logans-MacBook-Air.local>
2016-05-19 16:00:06 -0500

    big derp fix

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task('build', ['clean'], function(done){
     ['sass', 'html', 'fonts', 'scripts'],
     function(){
       buildBrowserify({
-        src: ['./app/app.ts', './node_modules/zone.js/dist/zone.js', './node_modules/reflect-metadata/Reflect.js', './typings/main.d.ts'],
+        src: ['./app/app.ts', './typings/main.d.ts'],
         minify: isRelease,
         browserifyOptions: {
           debug: !isRelease
